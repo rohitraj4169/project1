26389644ec8b4f8a2eca3e9b2746bb50bbb3a25a
rohitraj4169 <rohitraj@startbitsolutions.com>
2023-02-23 15:57:56 +0530

    all done

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :articles
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create1]
+end
